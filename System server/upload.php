c0d226d88af29d57394ef609a060dbef0fd090de
Ahmed Abdelhady <47082945+ahmedabdel-hady@users.noreply.github.com>
2020-10-04 20:13:37 +0200

    Space Xplores
    Nasa Space Apps Ismailia 2020

---
+++ System server/upload.php
@@ -0,0 +1,12 @@
+<?php
+
+$uploaddir = 'uploads/';
+
+$uploadfile = $uploaddir . basename($_FILES['image']['name']);
+
+if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile)) {
+    echo "Image succesfully uploaded.";
+} else {
+    echo "Image uploading failed.";
+}
+?> 
